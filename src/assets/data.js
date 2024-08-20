94acb18767b042b8ac963f7c8e5a422648829931
Sukumaraerrolla <sukumarjcct@gmail.com>
2024-08-20 18:59:39 +0530

    Quiz_App_Task

---
+++ src/assets/data.js
@@ -0,0 +1,87 @@
+export const data =[
+{
+  question:"Which type of JavaScript language is...",
+  option1:"Object-Oriented" ,
+  option2:"Object-Based" ,
+  option3:"Assembly-language" ,
+  option4: "High-level",
+  ans: 2,
+
+},
+{
+    question:"Which one of the following also known as Conditional Expression:",
+    option1:"Alternative to if-else",
+    option2:"Switch statement" ,
+    option3: "If-then-else statement",
+    option4: "immediate if ",
+    ans:4 ,
+},
+{
+    question:"In JavaScript, what is a block of statement?",
+    option1:"Conditional block" ,
+    option2: "block that combines a number of statements into a single compound statement",
+    option3: "both conditional block and a single statement",
+    option4:"block that contains a single statement" ,
+    ans:2 ,
+  
+  },
+  {
+      question:"When interpreter encounters an empty statements, what it will do:",
+      option1: "Shows a warning",
+      option2: "Prompts to complete the statement",
+      option3:"Throws an error" ,
+      option4:"Ignores the statements" ,
+      ans: 4,
+  },
+  {
+    question:"The 'function' and 'var' are known as:",
+    option1: "Keywords",
+    option2: "Data types",
+    option3:"Declaration statements" ,
+    option4: "Prototypes",
+    ans:3 ,
+  
+  },
+  {
+      question:"Which one of the following is the correct way for calling the JavaScript code?",
+      option1:"Preprocessor",
+      option2: "Triggering Event",
+      option3: "RMI",
+      option4: "Function/Method",
+      ans: 4,
+  },
+  {
+    question:"Which of the following type of a variable is volatile?",
+    option1:"Mutable variable" ,
+    option2: "Dynamic variable",
+    option3: "Volatile variable",
+    option4:"Immutable variable" ,
+    ans:1 ,
+  
+  },
+  {
+      question:"Which of the following option is used as hexadecimal literal beginning?",
+      option1: "00",
+      option2:"0x" ,
+      option3: "0X",
+      option4: "Both 0x and 0X",
+      ans:4 ,
+  },
+  {
+    question:"In the JavaScript, which one of the following is not considered as an error:",
+    option1: "Syntax error",
+    option2:"Missing of semicolons" ,
+    option3:"Division by zero" ,
+    option4: "Missing of Bracket",
+    ans: 3,
+  
+  },
+  {
+      question:" Which of the following number object function returns the value of the number?",
+      option1: "toString()",
+      option2:"valueOf()" ,
+      option3:"toLocaleString()" ,
+      option4: "toPrecision()",
+      ans: 2,
+  },
+];
